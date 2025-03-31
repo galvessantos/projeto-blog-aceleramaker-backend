3ee970371a1c5aba79df991c9a8093b24be61c60
galvessantos <gabrielww1@hotmail.com>
2025-03-31 20:52:39 -0300

    Configuracoes Iniciais

---
+++ src/main/java/aceleramaker/project/projeto01_blogpessoal/Projeto01BlogpessoalApplication.java
@@ -0,0 +1,13 @@
+package aceleramaker.project.projeto01_blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto01BlogpessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto01BlogpessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aceleramaker/project/projeto01_blogpessoal/Projeto01BlogpessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package aceleramaker.project.projeto01_blogpessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto01BlogpessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
