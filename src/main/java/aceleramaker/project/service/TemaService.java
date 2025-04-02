ff19594952544820a8114c3e161948fd3d54eb40
galvessantos <gabrielww1@hotmail.com>
2025-04-02 20:46:53 -0300

    Criando TemaService e método criar

---
+++ src/main/java/aceleramaker/project/service/TemaService.java
@@ -0,0 +1,20 @@
+package aceleramaker.project.service;
+
+import aceleramaker.project.dto.CreateTemaDto;
+import aceleramaker.project.entity.Tema;
+import aceleramaker.project.repository.TemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TemaService {
+
+    @Autowired
+    private TemaRepository temaRepository;
+
+    public Tema criar(CreateTemaDto dto) {
+        Tema tema = new Tema();
+        tema.setDescricao(dto.descricao());
+        return temaRepository.save(tema);
+    }
+}
