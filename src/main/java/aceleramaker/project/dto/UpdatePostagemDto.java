45ab0f27bd059f6fc1027e04cdc0585511554fc6
galvessantos <gabrielww1@hotmail.com>
2025-04-02 18:33:49 -0300

    Refatorando DTOs para usar record

--- src/main/java/aceleramaker/project/dto/UpdatePostagemDto.java
+++ src/main/java/aceleramaker/project/dto/UpdatePostagemDto.java
@@ -2,37 +2,8 @@ package aceleramaker.project.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public class UpdatePostagemDto {
-
-    @NotBlank
-    private String titulo;
-
-    @NotBlank
-    private String texto;
-
-    private Long temaId;
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getTexto() {
-        return texto;
-    }
-
-    public void setTexto(String texto) {
-        this.texto = texto;
-    }
-
-    public Long getTemaId() {
-        return temaId;
-    }
-
-    public void setTemaId(Long temaId) {
-        this.temaId = temaId;
-    }
-}
+public record UpdatePostagemDto(
+        @NotBlank String titulo,
+        @NotBlank String texto,
+        Long temaId
+) {}
