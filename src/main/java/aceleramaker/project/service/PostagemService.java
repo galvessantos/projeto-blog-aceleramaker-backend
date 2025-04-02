b5b7afc07670ead3c8396519fea881116f72124a
galvessantos <gabrielww1@hotmail.com>
2025-04-02 18:58:25 -0300

    Criando metodo criar no PostagemService

--- src/main/java/aceleramaker/project/service/PostagemService.java
+++ src/main/java/aceleramaker/project/service/PostagemService.java
@@ -1,7 +1,12 @@
 package aceleramaker.project.service;
 
+import aceleramaker.project.dto.CreatePostagemDto;
 import aceleramaker.project.entity.Postagem;
+import aceleramaker.project.entity.Tema;
+import aceleramaker.project.entity.Usuario;
 import aceleramaker.project.repository.PostagemRepository;
+import aceleramaker.project.repository.TemaRepository;
+import aceleramaker.project.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -13,6 +18,12 @@ public class PostagemService {
     @Autowired
     private PostagemRepository postagemRepository;
 
+    @Autowired
+    private TemaRepository temaRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     public List<Postagem> listarTodas() {
         return postagemRepository.findAll();
     }
@@ -23,5 +34,21 @@ public class PostagemService {
 
     public List<Postagem> buscarPorTitulo(String titulo) {
         return postagemRepository.findByTituloContainingIgnoreCase(titulo);
+    }
+
+    public Postagem criar(CreatePostagemDto dto) {
+        Postagem postagem = new Postagem();
+        postagem.setTitulo(dto.titulo());
+        postagem.setTexto(dto.texto());
+
+        Tema tema = temaRepository.findById(dto.temaId())
+                .orElseThrow(() -> new RuntimeException("Tema não encontrado"));
+        postagem.setTema(tema);
+
+        Usuario usuario = usuarioRepository.findById(dto.usuarioId())
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        postagem.setUsuario(usuario);
+
+        return postagemRepository.save(postagem);
     }
 }
