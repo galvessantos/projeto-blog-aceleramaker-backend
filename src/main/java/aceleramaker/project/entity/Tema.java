a8308b05d52718157e48aa269f2ebdecedbd8470
galvessantos <gabrielww1@hotmail.com>
2025-04-07 00:48:20 -0300

    docs: adicionando documentação Swagger na entidade Tema

--- src/main/java/aceleramaker/project/entity/Tema.java
+++ src/main/java/aceleramaker/project/entity/Tema.java
@@ -1,24 +1,31 @@
 package aceleramaker.project.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+
 import java.util.List;
 
 @Entity
 @Table(name = "tb_temas")
+@Schema(description = "Entidade que representa um tema de postagens no blog.")
 public class Tema {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Identificador único do tema.", example = "1")
     private Long id;
 
     @Column(name = "descricao", nullable = false)
+    @Schema(description = "Descrição do tema.", example = "Tecnologia")
     private String descricao;
 
     @OneToMany(mappedBy = "tema", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @Schema(description = "Lista de postagens associadas a este tema.")
     private List<Postagem> postagens;
 
     public Tema() {
     }
+
     public Tema(String descricao) {
         this.descricao = descricao;
     }
