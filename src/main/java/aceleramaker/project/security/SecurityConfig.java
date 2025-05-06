40f1b1bef633874b15478c2407e46bab4ff601e4
galvessantos <gabrielww1@hotmail.com>
2025-05-05 22:18:11 -0300

    refactor: connection sql

--- src/main/java/aceleramaker/project/security/SecurityConfig.java
+++ src/main/java/aceleramaker/project/security/SecurityConfig.java
@@ -87,7 +87,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:4200",
-                "https://projeto-blog-aceleramaker-frontend1.vercel.app"
+                "https://projeto-blog-aceleramaker-frontend1.vercel.app",
+                "https://projeto-blog-aceleramaker-frontend1.vercel.app/"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
